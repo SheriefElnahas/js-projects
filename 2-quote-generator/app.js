dca25f7d54fd36593d48d1debe57174995e85f4a
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-26 04:39:36 +0300

    Add Speech Syntehsis using JavScript

--- 2-quote-generator/app.js
+++ 2-quote-generator/app.js
@@ -1,1 +1,8 @@
-console.log('hey');
+const quote = document.querySelector('.card__quote');
+const btnSpeak = document.querySelector('.btn--speak');
+
+btnSpeak.addEventListener('click', () => {
+  const utterance = new SpeechSynthesisUtterance(quote.textContent);
+  speechSynthesis.speak(utterance);
+  
+})
