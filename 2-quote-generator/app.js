0f3c7e840fe2d6cb7183c52435a89424113cb61b
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-26 05:08:28 +0300

    copy to clipboard functionality

--- 2-quote-generator/app.js
+++ 2-quote-generator/app.js
@@ -1,8 +1,26 @@
 const quote = document.querySelector('.card__quote');
-const btnSpeak = document.querySelector('.btn--speak');
+const speakButton = document.querySelector('.btn--speak');
+const generateQuoteButton = document.querySelector('.btn--quote');
 
-btnSpeak.addEventListener('click', () => {
+speakButton.addEventListener('click', () => {
   const utterance = new SpeechSynthesisUtterance(quote.textContent);
   speechSynthesis.speak(utterance);
-  
-})
+
+  generateQuoteButton.disabled = true;
+
+  utterance.addEventListener('end', () => {
+    generateQuoteButton.disabled = false;
+  });
+});
+
+const copyButton = document.querySelector('.btn--copy');
+
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(quote.textContent).then(() => {
+    copyButton.classList.add('copied');
+
+    setTimeout(() => {
+      copyButton.classList.remove('copied');
+    }, 2000);
+  });
+});
