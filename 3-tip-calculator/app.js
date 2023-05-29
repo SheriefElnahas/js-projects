04c72eb6fdaa4cfd2669043736e0602ea870dc19
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-29 18:31:01 +0300

    extract logic into functions

--- 3-tip-calculator/app.js
+++ 3-tip-calculator/app.js
@@ -17,15 +17,36 @@ const calculatedTipElement = document.querySelector('#calculated-tip');
 const totalElement = document.querySelector('#total');
 
 billInput.addEventListener('input', (e) => {
-  // 1-get tip percent value
-  const tipPercentValue = parseInt(userTipPercent.textContent) / 100;
-  const billAmount = Number(e.target.value);
-  const calculatedTipValue = billAmount * tipPercentValue.toFixed(2);
+  // const tipPercentValue = parseInt(userTipPercent.textContent) / 100;
+  // const billAmount = Number(e.target.value);
+  // const calculatedTipValue = billAmount * tipPercentValue.toFixed(2);
 
-  const totalValue = billAmount + calculatedTipValue;
-  console.log(totalValue);
-  // console.log(+e.target.value)
-  calculatedTipElement.textContent = calculatedTipValue ;
-  totalElement.textContent = totalValue;
+  // const totalValue = billAmount + calculatedTipValue;
 
+  // calculatedTipElement.textContent = calculatedTipValue;
+  // totalElement.textContent = totalValue;
+
+  calculateTotal();
 });
+
+function calculateTotal() {
+  // 1- get the bill amount from the user
+
+  const billAmount = Number(billInput.value);
+
+  // 2-Get the tip percentage value
+  const tipPercentValue = parseInt(userTipPercent.textContent) / 100;
+
+  // 3- calculate tip value
+  const calculatedTipValue = Number((billAmount * tipPercentValue).toFixed(2));
+
+  // 4- calculate the total
+  const totalBill = (billAmount + calculatedTipValue).toFixed(2);
+  console.log(`bill amount : ${billAmount} - calculated tip value ${typeof calculatedTipValue} - total bill : ${totalBill}`);
+  updateUi(calculatedTipValue, totalBill);
+}
+
+function updateUi(calculatedTip, totalBill) {
+  calculatedTipElement.textContent = calculatedTip;
+  totalElement.textContent = totalBill;
+}
