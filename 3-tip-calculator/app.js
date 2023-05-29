8a616cf5cb25a1a8b78a2d79da49bf94ee610432
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-29 18:58:00 +0300

    tip range and split range functionality

--- 3-tip-calculator/app.js
+++ 3-tip-calculator/app.js
@@ -3,16 +3,26 @@ const tipRangeInput = document.querySelector('#tip');
 
 const splitNumberElement = document.querySelector('#split-output');
 const splitRangeInput = document.querySelector('#split');
+const splitPerPersonElement = document.querySelector('#split-per-person');
+
+const billInput = document.querySelector('#bill');
 
 tipRangeInput.addEventListener('input', (e) => {
   userTipPercent.textContent = e.target.value + '%';
+
+  if (billInput.value > 0) {
+    calculateTotal();
+  }
 });
 
 splitRangeInput.addEventListener('input', (e) => {
   splitNumberElement.textContent = e.target.value;
+
+  if (billInput.value > 0) {
+    calculateTotal();
+  }
 });
 
-const billInput = document.querySelector('#bill');
 const calculatedTipElement = document.querySelector('#calculated-tip');
 const totalElement = document.querySelector('#total');
 
@@ -26,12 +36,13 @@ billInput.addEventListener('input', (e) => {
   // calculatedTipElement.textContent = calculatedTipValue;
   // totalElement.textContent = totalValue;
 
-  calculateTotal();
+  if (e.target.value > 0) {
+    calculateTotal();
+  }
 });
 
 function calculateTotal() {
   // 1- get the bill amount from the user
-
   const billAmount = Number(billInput.value);
 
   // 2-Get the tip percentage value
@@ -42,11 +53,20 @@ function calculateTotal() {
 
   // 4- calculate the total
   const totalBill = (billAmount + calculatedTipValue).toFixed(2);
-  console.log(`bill amount : ${billAmount} - calculated tip value ${typeof calculatedTipValue} - total bill : ${totalBill}`);
-  updateUi(calculatedTipValue, totalBill);
+
+  // 5- get the split per people number
+  const splitNumber = Number(splitRangeInput.value);
+
+  updateUi(calculatedTipValue, totalBill, splitNumber);
 }
 
-function updateUi(calculatedTip, totalBill) {
+function updateUi(calculatedTip, totalBill, splitNumber) {
+  // 1- Update Tip Element
   calculatedTipElement.textContent = calculatedTip;
+
+  // 2- Update Total Element
   totalElement.textContent = totalBill;
+
+  // 3-Update Split Per Person Element
+  splitPerPersonElement.textContent = (totalBill / splitNumber).toFixed(2);
 }
