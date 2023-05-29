cdc2b3b90efe9614ac71ac5cd519698252631662
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-05-29 18:13:20 +0300

    calculate total

--- 3-tip-calculator/app.js
+++ 3-tip-calculator/app.js
@@ -1,13 +1,31 @@
-const tipPercent = document.querySelector('#tip-output');
+const userTipPercent = document.querySelector('#tip-output');
 const tipRangeInput = document.querySelector('#tip');
 
-const splitNumber = document.querySelector('#split-output');
+const splitNumberElement = document.querySelector('#split-output');
 const splitRangeInput = document.querySelector('#split');
 
 tipRangeInput.addEventListener('input', (e) => {
-  tipPercent.textContent = e.target.value + '%';
+  userTipPercent.textContent = e.target.value + '%';
 });
 
 splitRangeInput.addEventListener('input', (e) => {
-  splitNumber.textContent = e.target.value;
+  splitNumberElement.textContent = e.target.value;
+});
+
+const billInput = document.querySelector('#bill');
+const calculatedTipElement = document.querySelector('#calculated-tip');
+const totalElement = document.querySelector('#total');
+
+billInput.addEventListener('input', (e) => {
+  // 1-get tip percent value
+  const tipPercentValue = parseInt(userTipPercent.textContent) / 100;
+  const billAmount = Number(e.target.value);
+  const calculatedTipValue = billAmount * tipPercentValue.toFixed(2);
+
+  const totalValue = billAmount + calculatedTipValue;
+  console.log(totalValue);
+  // console.log(+e.target.value)
+  calculatedTipElement.textContent = calculatedTipValue ;
+  totalElement.textContent = totalValue;
+
 });
