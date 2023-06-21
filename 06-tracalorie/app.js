f89ccd8e0c49264866a7c548ab7f31e6a9df4e20
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-06-21 15:11:38 +0300

    add form data dynamically

--- 06-tracalorie/app.js
+++ 06-tracalorie/app.js
@@ -1,16 +1,63 @@
-const addMealBtn = document.querySelector('#add-meal');
+// ********************************************
+// ##   Add Meal & Add Workout Form Toggle   ##
+// ********************************************
+const addMealDataBtn = document.querySelector('#add-meal');
 const mealForm = document.querySelector('#meal-form');
 
-const addWorkoutBtn = document.querySelector('#add-workout');
+const addWorkoutDataBtn = document.querySelector('#add-workout');
 const workoutForm = document.querySelector('#workout-form');
 
-addMealBtn.addEventListener('click', () => {
-  mealForm.classList.remove('data__form--hide');
-  mealForm.classList.add('data__form--show');
+addMealDataBtn.addEventListener('click', () => {
+  mealForm.style.display = 'block';
+  mealOutput.style.display = 'none';
 });
 
-addWorkoutBtn.addEventListener('click', () => {
+addWorkoutDataBtn.addEventListener('click', () => {
+  workoutForm.style.display = 'block';
+  workoutOutput.style.display = 'none';
+});
 
-  workoutForm.classList.remove('data__form--hide');
-  workoutForm.classList.add('data__form--show');
+// **************************************************
+// ##   Add Meal Item & Add Workout Items Buttons  ##
+// **************************************************
+document.querySelectorAll('form').forEach((form) => form.addEventListener('submit', (e) => e.preventDefault()));
+
+const addMealItemBtn = document.querySelector('#add-meal-item');
+const mealOutput = document.querySelector('#output-meal');
+
+const mealItemInput = document.querySelector('#meal-item');
+const mealCaloriesInput = document.querySelector('#meal-calories');
+
+const outputMealValue = document.querySelector('#output-meal-item');
+const outputMealCalories = document.querySelector('#output-meal-calories');
+
+addMealItemBtn.addEventListener('click', () => {
+  outputMealValue.textContent = mealItemInput.value;
+  outputMealCalories.textContent = mealCaloriesInput.value;
+
+  mealItemInput.value = '';
+  mealCaloriesInput.value = '';
+
+  mealForm.style.display = 'none';
+  mealOutput.style.display = 'flex';
+});
+
+const addWorkoutBtn = document.querySelector('#add-workout-item');
+const workoutOutput = document.querySelector('#output-workout');
+
+const workoutItemInput = document.querySelector('#workout-item');
+const workoutCaloriesInput = document.querySelector('#workout-calories');
+
+const outputWorkoutValue = document.querySelector('#output-workout-item');
+const outputWorkoutCalories = document.querySelector('#output-workout-calories');
+
+addWorkoutBtn.addEventListener('click', () => {
+  outputWorkoutValue.textContent = workoutItemInput.value;
+  outputWorkoutCalories.textContent = workoutCaloriesInput.value;
+
+  workoutItemInput.value = '';
+  workoutCaloriesInput.value = '';
+
+  workoutForm.style.display = 'none';
+  workoutOutput.style.display = 'flex';
 });
